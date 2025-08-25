0665229e976f4c111e66fdefe5a2294faf29a43f
zcuss <zcusgt@gmail.com>
2025-08-25 23:56:57 +0700

    Update 1 .0

--- fix.lua
+++ fix.lua
@@ -3,8 +3,8 @@ local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/d
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 
 local Window = Fluent:CreateWindow({
-    Title = "Fluent " .. Fluent.Version,
-    SubTitle = "by dawid",
+    Title = "Barudak Fishit v 1.0",
+    SubTitle = "by Zcus",
     TabWidth = 160,
     Size = UDim2.fromOffset(580, 460),
     Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
